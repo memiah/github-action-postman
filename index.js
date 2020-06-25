4eb706a77fecf9cf4a528d6f5a2deaca7832de47
James Harding <jamesharding@memiah.co.uk>
2020-06-25 17:30:42 +0100

    Add basic action for calling postman monitor

---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const https = require('follow-redirects').https;
+//const fs = require('fs');
+
+try {
+    console.log(core.getInput('POSTMANAPIKEY'));
+
+    var options = {
+        'method': 'POST',
+        'hostname': 'api.getpostman.com',
+        'path': '/monitors/1eab6268-40dc-4990-b999-5699b299ad7c/run',
+        'headers': {
+          'X-Api-Key': ''
+        },
+        'maxRedirects': 20
+      };
+      
+      var req = https.request(options, function (res) {
+        var chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function (chunk) {
+          var body = Buffer.concat(chunks);
+          if (res.statusCode == 200) {
+            console.log(body.toString());
+          } else {
+            core.setFailed(`Failed. Error code ${res.statusCode}`);
+            console.error(body.toString());
+          }
+        });
+      
+        res.on("error", function (error) {
+          console.error(error);
+        });
+      });
+      
+      req.end();
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+
+
